810d6710c43cce7e6cc62d3516b2b0f8308153ff
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-10-05 14:29:07 +0100

    Added indexes for foreign keys in medical_treatments table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -63,3 +63,6 @@ CREATE INDEX idx_fk_patient_id ON medical_histories (patient_id);
 CREATE INDEX idx_fk_medical_history_id ON invoices (medical_history_id);
 CREATE INDEX idx_fk_invoice_id ON invoice_items (invoice_id);
 CREATE INDEX idx_fk_treatment_id ON invoice_items (treatment_id);
+
+CREATE INDEX idx_fk_medical_history_id_mt ON medical_treatments (medical_history_id);
+CREATE INDEX idx_fk_invoice_id_mt ON medical_treatments (invoice_id);
